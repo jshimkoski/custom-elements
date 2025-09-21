eb6620b182c8845370ffc1e1a9f2f3844fdf468f
Jason Shimkoski <jshimkoski@me.com>
2025-09-21 13:15:55 -0400

    fix: add missing overflow classes

--- src/lib/runtime/style.ts
+++ src/lib/runtime/style.ts
@@ -326,6 +326,14 @@ export const utilityMap: CSSMap = {
   "overflow-hidden": "overflow:hidden;",
   "overflow-visible": "overflow:visible;",
   "overflow-scroll": "overflow:scroll;",
+  "overflow-y-auto": "overflow-y:auto;",
+  "overflow-y-hidden": "overflow-y:hidden;",
+  "overflow-y-visible": "overflow-y:visible;",
+  "overflow-y-scroll": "overflow-y:scroll;",
+  "overflow-x-auto": "overflow-x:auto;",
+  "overflow-x-hidden": "overflow-x:hidden;",
+  "overflow-x-visible": "overflow-x:visible;",
+  "overflow-x-scroll": "overflow-x:scroll;",
 
   /* Pointer Events */
   "pointer-events-none": "pointer-events:none;",
